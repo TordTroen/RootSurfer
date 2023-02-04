e98a2c3925cd6c1221ef3d668427e6f392263682
TORDT-PC1\tordt <tordtroen@gmail.com>
2023-02-04 13:17:39 +0100

    - Made custom blueprint for player character - Renamed Test map

--- Source/RootSurfer/RootSurferGameMode.cpp
+++ Source/RootSurfer/RootSurferGameMode.cpp
@@ -8,7 +8,7 @@ ARootSurferGameMode::ARootSurferGameMode()
 	: Super()
 {
 	// set default pawn class to our Blueprinted character
-	static ConstructorHelpers::FClassFinder<APawn> PlayerPawnClassFinder(TEXT("/Game/FirstPerson/Blueprints/BP_FirstPersonCharacter"));
+	static ConstructorHelpers::FClassFinder<APawn> PlayerPawnClassFinder(TEXT("/Game/Blueprints/BP_SurfPlayerCharacter"));
 	DefaultPawnClass = PlayerPawnClassFinder.Class;
 
 }
