113d6756efed403288acd124ba55185357eba48e
Ole Andreas <oleandreas86@gmail.com>
2023-02-05 12:15:31 +0100

    Revert "Test"
    This reverts commit 0964ca974a26bb6ecdd2f87458e0f37f04578837.

--- Source/RootSurfer/RootSurferGameMode.cpp
+++ Source/RootSurfer/RootSurferGameMode.cpp
@@ -3,7 +3,6 @@
 #include "RootSurferGameMode.h"
 #include "RootSurferCharacter.h"
 #include "UObject/ConstructorHelpers.h"
-#include "Kismet/GameplayStatics.h"
 
 ARootSurferGameMode::ARootSurferGameMode()
 	: Super()
@@ -12,23 +11,4 @@ ARootSurferGameMode::ARootSurferGameMode()
 	static ConstructorHelpers::FClassFinder<APawn> PlayerPawnClassFinder(TEXT("/Game/Blueprints/BP_SurfPlayerCharacter"));
 	DefaultPawnClass = PlayerPawnClassFinder.Class;
 
-	PrimaryActorTick.bCanEverTick = true;
-
-}
-
-void ARootSurferGameMode::StartPlay()
-{
-	Super::StartPlay();
-	m_TrackingPlayer = UGameplayStatics::GetPlayerController(this, 0);
-}
-
-void ARootSurferGameMode::Tick(float DeltaTime)
-{
-	if (m_TrackingPlayer != nullptr)
-	{
-		ARootSurferCharacter* Character = Cast<ARootSurferCharacter>(m_TrackingPlayer->GetCharacter());
-		//m_CurrentScore += m_ScorePerSecond * DeltaTime;
-		const float Speed = Character->GetVelocity().Length();
-		m_CurrentScore += Speed * m_ScoreSpeedModifier * DeltaTime;
-	}
 }
--- Source/RootSurfer/RootSurferGameMode.h
+++ Source/RootSurfer/RootSurferGameMode.h
@@ -13,20 +13,6 @@ class ARootSurferGameMode : public AGameModeBase
 
 public:
 	ARootSurferGameMode();
-	virtual void StartPlay() override;
-	virtual void Tick(float DeltaTime) override;
-
-	UPROPERTY(EditDefaultsOnly, Category = "Surf")
-	float m_ScorePerSecond = 10.0f;
-
-	UPROPERTY(EditDefaultsOnly, Category = "Surf")
-	float m_ScoreSpeedModifier = 1.0f;
-
-
-	UPROPERTY(BlueprintReadWrite, Category = "Surf")
-	float m_CurrentScore = 0.0f;
-
-	APlayerController* m_TrackingPlayer = nullptr;
 };
 
 
